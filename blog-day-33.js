ca0bd9a9e7cfd59b0ce23d72c1edf1f7ad711179
Prem Shankar Rai <premshankarrai2005@gmail.com>
2025-06-17 21:32:22 +0530

    blog-day-33.js

---
+++ blog-day-33.js
@@ -0,0 +1,9 @@
+//  📝 Day 31: Day 33: Find the output of the this problem using javascript?
+                // let a = 11, b = 22;
+                // let c = a + b + a++ + b++ + ++a + ++b
+                // console.log(a, b, c);
+// Solution:-
+
+let a = 11, b = 22;
+let c = a + b + a++ + b++ + ++a + ++b
+console.log(a, b, c); // a = 13 ,b = 24,c = 105
